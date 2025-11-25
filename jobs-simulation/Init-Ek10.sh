fc457c8759f3738c3d9a2181d51d047aca3a16ca
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2025-11-25 16:36:42 -0500

    Initialisation for different cooling rates

---
+++ jobs-simulation/Init-Ek10.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=Init-Ek10
+#SBATCH --output=../scratch/logs/Strain-%j.txt
+
+module load julia/1.10.10
+
+# Launch from scratch
+export JULIA_DEPOT_PATH=$SCRATCH/julia-trig
+cd ~/turbulence-at-many-fronts
+
+output_folder=$SCRATCH/turbulence-at-many-fronts/Init-Ek10
+
+run_time="8e5"
+start_time="-8e5"
+save_time="1e4"
+
+f="1e-4"
+L="1e3"
+
+betax=20
+betah=3
+
+Nx=1024
+Nh=768
+Ny=128
+Nz=128
+
+betab=1
+betal=6
+betaH=0.1
+
+Roalpha=0
+Ek=1.0
+beta0=8
+
+comment="Initialized front with Ek = 1.0"
+
+julia -t 24 -- src-simulation/simulation.jl $output_folder $run_time $start_time $save_time $f $L $betax $betah $Nx $Nh $Ny $Nz $betab $betal $betaH $Roalpha $Ek $beta0 $comment
