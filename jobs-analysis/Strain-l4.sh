1eb21a6fc41ffc84aec5cc7e05ec514107bd0348
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2025-11-25 12:25:49 -0500

    Jobs for strain

---
+++ jobs-analysis/Strain-l4.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=192
+#SBATCH --time=4:00:00
+#SBATCH --job-name=ppStrain-l4
+#SBATCH --output=../scratch/logs/ppStrain-l4.txt
+
+module load julia/1.10.10
+
+# Copy installation to RAM disk
+echo "Copying installation to RAM disk"
+export RAM=/dev/shm/Strain-l4
+mkdir $RAM
+
+cp -r $HOME/turbulence-at-many-fronts/.julia-tri $RAM
+
+# Launch from RAM disk
+echo "Running..."
+export JULIA_DEPOT_PATH=$RAM/.julia-tri
+export JULIA_SCRATCH_TRACK_ACCESS=0
+cd ~/turbulence-at-many-fronts
+
+# Location of output.jld2
+export SIM_OUTPUT_FOLDER=../scratch/turbulence-at-many-fronts/Strain-l4
+julia -t 192 -- src-analysis/postprocess/postprocess.jl $SIM_OUTPUT_FOLDER DFM $RAM
+julia -t 192 -- src-analysis/postprocess/postprocess.jl $SIM_OUTPUT_FOLDER PV $RAM
+julia -t 192 -- src-analysis/postprocess/postprocess.jl $SIM_OUTPUT_FOLDER RI $RAM
+julia -t 192 -- src-analysis/postprocess/postprocess.jl $SIM_OUTPUT_FOLDER TKE $RAM
+
+rm $RAM -rf
