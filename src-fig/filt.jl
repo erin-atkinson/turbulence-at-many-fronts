b0ea402c188a35428650da5aa3dd7273f59c8a5f
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2025-11-28 17:29:02 -0500

    Allow different kernel widths

--- src-fig/filt.jl
+++ src-fig/filt.jl
@@ -1,6 +1,8 @@
 # Shortcut for filtering...
 @inline filt(a, σ::Tuple) = imfilter(a, gaussian(σ))
+@inline filt(a, σ::Tuple, l) = imfilter(a, gaussian(σ, l))
+
 @inline filt(a, σ...) = filt(a, σ)
 
 # If only one σ, repeat for length of a
-@inline filt(a::A, σ::Integer) where {T, n, A<:Array{T, n}} = filt(a, (repeat([σ], n)..., ))
+@inline filt(a::A, σ::Real) where {T, n, A<:Array{T, n}} = filt(a, (repeat([σ], n)..., ))
