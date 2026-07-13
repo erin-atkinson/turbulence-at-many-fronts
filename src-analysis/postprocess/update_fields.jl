68adf57d7453da21ceb75081967daff5f0445add
Erin Atkinson <erin.atkinson60@gmail.com>
2026-07-13 18:46:59 -0400

    General fields

--- src-analysis/postprocess/update_fields.jl
+++ src-analysis/postprocess/update_fields.jl
@@ -1,12 +1,11 @@
 function update_fields!(fields, fds, clock, frame; skip_update)
 
-    # Set current state
-    :u in skip_update || set!(fields.u, fds.u[frame])
-    :v in skip_update || set!(fields.v, fds.v[frame])
-    :w in skip_update || set!(fields.w, fds.w[frame])
-    :b in skip_update || set!(fields.b, fds.b[frame])
-    :pNHS in skip_update || set!(fields.pNHS, fds.pNHS[frame])
-
+    fieldnames = Symbol.(keys(fds.fields))
+    for fieldname in fieldnames
+        fieldname ∈ skip_update && continue
+        set!(fields[fieldname], fds[fieldname][frame])
+    end
+    
     # Set next state
     l = length(fds.u)
     f2 = min(frame + 1, l)
@@ -14,7 +13,6 @@ function update_fields!(fields, fds, clock, frame; skip_update)
     :v_next in skip_update || set!(fields.v_next, fds.v[f2])
     :w_next in skip_update || set!(fields.w_next, fds.w[f2])
     :b_next in skip_update || set!(fields.b_next, fds.b[f2])
-    :pNHS_next in skip_update || set!(fields.pNHS_next, fds.pNHS[f2])
 
     compute_background!(fields.U, fields.V, fields.W, clock)
 
